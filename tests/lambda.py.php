cb50add7a2afb3cf281d6765ad28923e2fe2bb21
danda <danda@osc.co.cr>
2015-12-05 15:57:14 -0800

    fix formatting of lambda functions.  single line

--- tests/lambda.py.php
+++ tests/lambda.py.php
@@ -1,12 +1,6 @@
 <?php
 require_once('py2phplib.php');
-$result = function ($x) {
-    return     ($x * 2)
-;
-};
+$result = function ($x) {return ($x * 2);};
 pyjslib_printFunc($result(2));
-pyjslib_printFunc(call_user_func(function ($x) {
-    return     ($x * 2)
-;
-}, 3));
+pyjslib_printFunc(call_user_func(function ($x) {return ($x * 2);}, 3));
 
