86ebe098f4ff3049a28426b595d613963975e59a
danda <danda@osc.co.cr>
2015-12-05 19:02:04 -0800

    Added support and test case for exceptions.  try, catch, finally

---
+++ tests/exceptions.py
@@ -0,0 +1,30 @@
+class ZeroDivisionException(Exception):
+    pass
+
+class ValidationException(Exception):
+    pass
+
+
+def div(x,y):
+    try:
+        if x < 0 or y < 0:
+            raise ValidationException
+        
+        if y == 0:
+            raise ZeroDivisionException
+        
+        result = x / y
+        
+    except ZeroDivisionException:
+        print "division by zero!"
+    except ValidationException:
+        print "Values must be greater than 0!"
+#   else clause not supported in PHP or py2php
+#    else:
+#        print "result is", result
+    finally:
+        print "executing finally clause"
+        
+div(-1,3)
+div(4,0)
+div(0,4)
---
+++ tests/exceptions.py.php
@@ -0,0 +1,30 @@
+<?php
+require_once('py2phplib.php');
+class ZeroDivisionException extends Exception {
+}
+class ValidationException extends Exception {
+}
+function div($x,$y) {
+    try {
+        if (($x < 0) || ($y < 0)) {
+            throw new ValidationException;
+        }
+        if (($y == 0)) {
+            throw new ZeroDivisionException;
+        }
+        $result = ($x / $y);
+    }
+    catch(ZeroDivisionException $e) {
+        pyjslib_printFunc('division by zero!');
+    }
+    catch(ValidationException $e) {
+        pyjslib_printFunc('Values must be greater than 0!');
+    }
+    finally {
+        pyjslib_printFunc('executing finally clause');
+    }
+}
+div(-1, 3);
+div(4, 0);
+div(0, 4);
+
