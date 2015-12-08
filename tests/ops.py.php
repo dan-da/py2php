542e512bfb5c7e555c15995b214b439a839c1759
danda <danda@osc.co.cr>
2015-12-07 19:54:06 -0800

    several fixes and additions, plus new tests

---
+++ tests/ops.py.php
@@ -0,0 +1,14 @@
+<?php
+require_once('py2phplib.php');
+$a = 6 << 2;
+$b = 6 >> 2;
+$c = 1 | 2;
+$d = 1 & 2;
+$e = 2 ^ 5;
+$f = (2 * 6);
+$g = pow(2, 8);
+pyjslib_printFunc(array($a, $b, $c, $d, $e, $f, $g), true);
+$a |= 2;
+$b &= 2;
+pyjslib_printFunc(array($a, $b), true);
+
