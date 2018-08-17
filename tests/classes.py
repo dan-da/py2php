b5874b8b1d919a0957fbd9cbc7d6d1a4518302af
danda <danda@osc.co.cr>
2018-08-17 14:40:58 +0800

    translate python 'object' to php 'stdClass' when inheriting

---
+++ tests/classes.py
@@ -0,0 +1,5 @@
+# note: object is python base object, like stdObject in php.
+# inherititng from it is redundant, but should not break translated php.
+class myclass(object):
+    def __init__(self):
+        pass
---
+++ tests/classes.py.php
@@ -0,0 +1,8 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . DIRECTORY_SEPARATOR . 'libpy2php');
+require_once('libpy2php.php');
+class myclass extends stdClass {
+    function __construct() {
+    }
+}
+
