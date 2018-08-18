116c19591aeca9e4ede8c83a5f8439c6be18e1eb
danda <danda@osc.co.cr>
2018-08-18 12:49:38 +0800

    translate 'for k,v in map.itemiter()' to 'foreach($map as $k => $v'

---
+++ tests/formap.py
@@ -0,0 +1,14 @@
+map = {"fname": "woody", "lname": "woodpecker", "species": "bird", "gender": "male"}
+
+map2 = {}
+
+# python dicts are unsorted, so if we just print inside the foreach
+# then php output will differ.
+for k,v in map.iteritems():
+    map2[k] = v
+    
+print map2["fname"]
+print map2["lname"]
+print map2["species"]
+print map2["gender"]
+
---
+++ tests/formap.py.php
@@ -0,0 +1,13 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . DIRECTORY_SEPARATOR . 'libpy2php');
+require_once('libpy2php.php');
+$map = ['fname' => 'woody', 'lname' => 'woodpecker', 'species' => 'bird', 'gender' => 'male'];
+$map2 = [];
+foreach( pyjslib_list($map) as $k => $v ) {
+    $map2[$k] = $v;
+}
+pyjslib_printnl($map2['fname']);
+pyjslib_printnl($map2['lname']);
+pyjslib_printnl($map2['species']);
+pyjslib_printnl($map2['gender']);
+
